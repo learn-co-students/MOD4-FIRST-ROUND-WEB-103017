9b19b0e43387a2e0533894e10b3e17cbbd36409a
pawlkris <paulkristapovich@gmail.com>
2018-01-17 10:34:53 -0500

    pk solution

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,19 +11,30 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
+    this.state = {
+      transactions: transactions,
+      value : ""
+    }
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(res => res.json())
+      .then(json => this.setState({transactions:json}))
   }
 
   handleChange(event) {
-    // your code here
+    this.setState({value: document.getElementById("searchBar").value}, () => console.log(this.state))
   }
 
   render() {
-
+    // would then set a filteredTransactions array here by filtering through transaction array looking for matching text (i.e. .filter(transaction => transaction.description.includes(this.state.value) || transaction.category.includes(this.state.value)))
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} value={this.state.value}/>
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,36 @@
 import React from 'react'
 
-const Search = () => {
+
+
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
+        id="searchBar"
         placeholder={"Search your Recent Transactions"}
       />
-      <i className="circular search link icon"></i>
+      <i onClick={() => props.handleChange(event)}className="circular search link icon"></i>
     </div>
   )
 }
 
 export default Search
+
+
+
+// const Search = (props) => {
+//   return (
+//     <div className="ui huge fluid icon input">
+//       <input
+//         type="text"
+//         onChange={() => props.handleChange(event)}
+//         placeholder={"Search your Recent Transactions"}
+//         value={props.value}
+//       />
+//       <i className="circular search link icon"></i>
+//     </div>
+//   )
+// }
+//
+// export default Search
