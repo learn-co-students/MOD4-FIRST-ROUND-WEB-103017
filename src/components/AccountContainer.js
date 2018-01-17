d6b565b2d6e7b6c4bf55ffe0bab2f41a2156a3f1
dansbands <dansbands@hotmail.com>
2018-01-17 10:37:41 -0500

    got stuck

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,19 +11,38 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
+    this.state = {
+      transactions: transactions
+      filter: ""
+    }
+  }
 
+  // componentDidMount() {
+  //   this.setState({
+  //     transactions: transactions
+  //   })
+  // }
+  filterTransactions = () => {
+    tranactions.filter(t => {
+      return(
+        this.state.transactions.description === this.state.filter || this.state.transactions.catgory === this.state.filter 
+      )
+    })
   }
 
   handleChange(event) {
     // your code here
+    this.setState({
+      filter: event.target.value
+    });
   }
 
   render() {
-
+    console.log("AccountContainer:", this.state.transactions);
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     )
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,37 +1,53 @@
-import React from 'react'
+import React from 'react';
+import Transaction from './Transaction';
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+ let transactions = props.transactions
+ let newTrans
+ console.log("TL", props);
+
+ newTrans = transactions.map((trans) => {
+   return(
+     <Transaction
+       data={trans}
+       />
+   );
+  })
+
+console.log("TL:", newTrans);
 
   return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">
+                Posted At
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Description
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Category
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Amount
+              </h3>
+            </th>
+          </tr>
+          { newTrans }
 
-        {"... your code here..."}
-
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    //   <div>
+    //   // <Transaction  />
+    // </div>
   )
 }
 
