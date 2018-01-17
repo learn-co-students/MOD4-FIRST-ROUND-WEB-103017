10100f22767d5be0b4dd6589b6ac50e97e73bab5
George Gore <georgelgore@gmail.com>
2018-01-17 10:31:51 -0500

    Submit Test

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,53 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
+    super();
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+    this.state = {
+      transactions: [],
+      typed: "",
+      toDisplay: []
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    // your code here
+    let toEnter = event.target.value;
+    let toShow = transactions.filter(
+      trans =>
+        trans.category.toLowerCase().includes(event.target.value) ||
+        trans.description.toLowerCase().includes(this.state.typed)
+    );
+    console.log(toEnter, toShow);
+    this.setState({ typed: toEnter }, console.log(this.state.typed));
+    this.setState({ toDisplay: toShow });
+  }
+
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(resp => resp.json())
+      .then(obj => {
+        this.setState({ transactions: obj });
+        this.setState({ toDisplay: obj });
+      });
   }
 
   render() {
-
+    console.log(this.state.toDisplay, "1");
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} typed={this.state.typed} />
+        <TransactionsList
+          transactions={this.state.toDisplay}
+          original={this.state.transactions}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
