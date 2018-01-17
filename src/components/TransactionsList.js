58eb99f73876cd60d1482cabfb4411071377e768
github username <github email address>
2018-01-17 10:31:56 -0500

    Finished Code Challenge

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  const transactions = props.searchTerm ? props.transactions.filter((transaction, i) =>
+    transaction.description.toUpperCase().includes(props.searchTerm.toUpperCase()) || transaction.category.toUpperCase().includes(props.searchTerm.toUpperCase())
+  ) : props.transactions
+
+  // const transactionRows = transactions.map((transaction, i) => < Transaction transaction={transaction} key={i} />)
+
+  // The code below accounts for a long fetch request. Since I have my this.state.transaction set to an empty array, I can still map over it with no problem, but not results to the user. For some reason if the fetch request fails, this message on the falsey side of the ternary will be rendered to the screen.
+  const transactionRows = props.transactions.length ? transactions.map((transaction, i) => < Transaction transaction={transaction} key={i} />) : <tr><td>...taking a while to get your money</td></tr>
+
+  //With a litte more time, I would display a message saying 'No results' to the user after running through the filter and account for more user input errors besides just checking the case of the characters. 
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +39,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {transactionRows}
 
       </tbody>
     </table>
