e55c0248ea24636a01d56e60bc280b4b0b495a35
github username <github email address>
2018-01-17 10:34:17 -0500

    plz read note line 12 TransactionList

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-const Search = () => {
+// const Search = () => {
+export default class Search extends React.Component {
+  handleInput = (e) => {
+    let term = e.target.value
+    this.props.callBackFromParent(term)
+  }
+
+  render(){
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
+        onChange={this.handleInput}
         placeholder={"Search your Recent Transactions"}
       />
       <i className="circular search link icon"></i>
     </div>
-  )
+  )}
 }
 
-export default Search
+// export default Search
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,75 @@
 import React from 'react'
 
-const TransactionsList = () => {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+export default class TransactionsList extends React.Component {
 
-        {"... your code here..."}
 
-      </tbody>
-    </table>
-  )
+
+searchMatcher = (type) => {
+  let newArr = []
+
+
+  //was going to filter with search term here but didnt have time
+
+  
+  this.props.results.map(result => {
+    result.category.indexOf(this.props.results.term) > -1
+  })
+
 }
 
-export default TransactionsList
+
+render(){
+
+
+  const dates = (this.props.results && this.props.term)
+    ? this.props.results.map(result => <dl>{result.posted_at}</dl>)
+    : <dl></dl>
+
+  const description = (this.props.results && this.props.term)
+    ? this.props.results.map(result => <dl>{result.description}</dl>)
+    : <dl></dl>
+
+  const category = (this.props.results && this.props.term)
+      ? this.props.results.map(result => <dl>{result.category}</dl>)
+      : <dl></dl>
+
+  const amount = (this.props.results && this.props.term)
+      ? this.props.results.map(result => <dl>{result.amount}</dl>)
+      : <dl></dl>
+
+return (
+  <table className="ui celled striped padded table">
+    <tbody>
+      <tr>
+        <th>
+          <h3 className="ui center aligned header">
+            Posted At
+          </h3>
+          <ul>{dates}</ul>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">
+            Description
+          </h3>
+          <ul>{description}</ul>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">
+            Category
+          </h3>
+          <ul>{category}</ul>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">
+            Amount
+          </h3>
+          <ul>{amount}</ul>
+        </th>
+      </tr>
+    </tbody>
+  </table>
+)}
+}
+
+// export default TransactionsList
