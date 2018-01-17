718f145ce7125236f2e685fa2b9c680e56f0bf22
jjwumaster <jjwumaster@gmail.com>
2018-01-17 10:31:54 -0500

    final submission

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,61 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
+    super();
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+    this.state = {
+      transactions: [],
+      filteredTransactions: []
+    };
 
+    this.handleChange = this.handleChange.bind(this); // why do i need this
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    this.fetchTransactions();
+  }
+
+  fetchTransactions() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(resp => resp.json())
+      .then(json =>
+        this.setState({ transactions: json, filteredTransactions: json })
+      );
+  }
+
+  handleChange(e) {
+    const query = e.target.value.toLowerCase().trim();
+
+    const newTransactions = this.state.transactions.filter(transaction => {
+      // how to pull this into its own function?? related to the above "this" binding question
+      const inDescription = transaction.description
+        .toLowerCase()
+        .includes(query);
+      const inCategory = transaction.category.toLowerCase().includes(query);
+
+      return inDescription || inCategory;
+    });
+
+    this.setState({
+      filteredTransactions: newTransactions
+    });
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} transactions={transactions} />
+        <TransactionsList
+          transactions={this.state.filteredTransactions}
+          handleChange={this.handleChange}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
