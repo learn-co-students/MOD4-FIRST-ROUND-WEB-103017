2213f62be49d021019b42ab828a64823ad6182f4
crutell331 <tcw331@gmail.com>
2018-01-17 10:30:57 -0500

    complete

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,41 @@
-import React from 'react'
-
-const TransactionsList = () => {
-
+import React from "react";
+import Transaction from "./Transaction";
+const TransactionsList = props => {
+  const transactions = props.transactions.map(transaction => {
+    let description = transaction.description.toLowerCase();
+    let category = transaction.category.toLowerCase();
+    let searchTerm = props.searchTerm.toLowerCase();
+    return description.includes(searchTerm) ||
+      category.includes(searchTerm) ||
+      props.searchTerm === "" ? (
+      <Transaction
+        transaction={transaction}
+        id={transaction.id}
+        key={transaction.id}
+      />
+    ) : null;
+  });
   return (
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
+            <h3 className="ui center aligned header">Posted At</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-
-        {"... your code here..."}
-
+        {transactions}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
