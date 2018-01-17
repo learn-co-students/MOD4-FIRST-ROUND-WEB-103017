17371cc15a307856d1e36d92b62de59bbe1cfa7c
psarma89 <priyam.sarma@gmail.com>
2018-01-17 10:00:54 -0500

    Adds all the new code

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
@@ -11,19 +11,60 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
+    this.state ={
+      transactions: [],
+      searchTerm: ''
+    }
 
   }
 
-  handleChange(event) {
+  getAPIData = () => {
+    return fetch('https://boiling-brook-94902.herokuapp.com/transactions').then(resp => resp.json())
+  }
+
+  componentDidMount(){
+    this.getAPIData().then(json => {
+      this.setState({
+        transactions: json
+      })
+    })
+  }
+
+  check
+
+  filterTransactions = () =>{
+    const currentSearchTerm = this.state.searchTerm;
+    if (currentSearchTerm) {
+      this.getAPIData().then(json => {
+        const filteredTransactions = json.filter(trn => {
+          return (trn.description.toLowerCase().includes(currentSearchTerm.toLowerCase()) || trn.category.toLowerCase().includes(currentSearchTerm.toLowerCase()))
+        })
+        this.setState({
+          transactions: filteredTransactions
+        })
+      })
+    }else {
+      this.getAPIData().then(json => {
+        this.setState({
+          transactions: json
+        })
+      })
+    }
+  }
+
+  handleChange = (event) => {
     // your code here
+    this.setState({
+      searchTerm: event.target.value
+    }, this.filterTransactions)
   }
 
   render() {
-
+    const {transactions, searchTerm} = this.state
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} searchTerm={searchTerm}/>
+        <TransactionsList transactions={transactions}/>
       </div>
     )
   }
