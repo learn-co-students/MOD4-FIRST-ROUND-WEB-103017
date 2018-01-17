66dabf1a625e88766d2d9f74d8a92d4663b8500e
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2018-01-17 10:31:02 -0500

    Done.

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,23 +7,34 @@ class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      transactions: transactions,
+      filter: ""
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
+
+  // componentDidMount(){
+  //   fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+  //   .then(resp=>resp.json())
+  //   .then(json => this.setState(transactions: json))
+  // }
+
   handleChange(event) {
-    // your code here
+    let value = event.target.value
+    console.log("state", this.state);
+    this.setState({filter: value})
   }
 
   render() {
-
+    console.log("in app", this.state)
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} filter={this.state.filter}/>
+        <TransactionsList transactions = {this.state.transactions}/>
       </div>
     )
   }
